3f1b0034589c3a8599723457c38752f3b5680729
seriva <11056665+seriva@users.noreply.github.com>
2026-04-16 14:47:37 +0200

    feat: add math package with Add, Mul, and Square functions in unusedimport and withdotimport directories

---
+++ test/fixtures/multifile/unusedimport/mathpkg/math.go
@@ -0,0 +1,9 @@
+package math
+
+func Add(a int, b int) int {
+    return a + b
+}
+
+func Mul(a int, b int) int {
+    return a * b
+}
---
+++ test/fixtures/multifile/withdotimport/mathpkg/extra.go
@@ -0,0 +1,5 @@
+package math
+
+func Square(n int) int {
+    return Mul(n, n)
+}
